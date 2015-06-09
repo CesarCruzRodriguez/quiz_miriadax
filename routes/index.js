255ff0057220431d4d79a62954699117566f8df0
Cesar Cruz <cesarcruzrodriguez@gmail.com>
2015-06-09 10:51:44 +0100

    editar preguntas

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,8 @@ router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
 //nueva pregunta
 router.get('/quizes/new',                   quizController.new);
 router.post('/quizes/create',               quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',    quizController.edit);
+router.put('/quizes/:quizId(\\d+)',         quizController.update);
 
 router.get('/quizes/search',                quizController.search);
 
